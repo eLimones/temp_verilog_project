f3b1590357b0f8b5d85ef1033899141de7e0227f
Manuel Limones <limonesu.me@gmail.com>
2017-04-18 21:11:18 -0500

    Add branch history table to 1-Bit branch predictor

--- source/one_bit_branch_prediction.v
+++ source/one_bit_branch_prediction.v
@@ -2,18 +2,24 @@
 /*
     One bit branch prediction
 */
-module one_bit_branch_prediction(
+module one_bit_branch_prediction #(parameter address_width = 1)(
     input clk,
     input rst,
+    input [address_width-1:0] branch_address,
     input branch_result,
     output reg prediction
     );
 
+reg branch_history_table [(2**address_width)-1:0];
+
 always@(posedge clk)begin
-    if(rst)
+    if(rst)begin
         prediction <= 0;
-    else
-        prediction <= branch_result;
+    end
+    else begin
+        branch_history_table[branch_address] <= branch_result;
+        prediction <= branch_history_table[branch_address];
+    end
 end
 
 endmodule
